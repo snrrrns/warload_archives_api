4918b702d01a6fed681065983e6ab9507ee87d19
suguru nakagawa <s.nakagawa.n4s3@gmail.com>
2024-04-07 16:34:52 +0900

    add: get figure detail api

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
 import { Hono } from 'hono';
+import { prettyJSON } from 'hono/pretty-json';
+import { logger } from 'hono/logger';
+import { Bindings } from './types/db';
+import api from './api';
 
-const app = new Hono();
+const app = new Hono<{ Bindings: Bindings }>();
+app.use('*', prettyJSON());
+app.use(logger());
+
+app.route('/api', api);
 
 app.get('/', (c) => {
   return c.text('Hello Hono!');
