e4cbbce297cfc936bd11c3f9317b933fa9e2f9fe
suguru nakagawa <s.nakagawa.n4s3@gmail.com>
2024-05-05 14:36:49 +0900

    chore: setup eslint completed

---
+++ eslint.config.mjs
@@ -0,0 +1,14 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+export default [
+  { ignores: ['node_modules', '.wrangler'] },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReactConfig,
+  eslintConfigPrettier,
+];
