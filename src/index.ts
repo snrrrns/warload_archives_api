563db62ade1b9e132b0608adea647209413456ce
suguru nakagawa <s.nakagawa.n4s3@gmail.com>
2024-05-06 10:38:24 +0900

    fix: src/index.ts | bindings path

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { Hono } from 'hono';
 import { prettyJSON } from 'hono/pretty-json';
 import { logger } from 'hono/logger';
-import { Bindings } from './types/bindings';
+import { Bindings } from './bindings';
 import api from './api';
 
 const app = new Hono<{ Bindings: Bindings }>();
